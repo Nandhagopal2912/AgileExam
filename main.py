f7cc6f7b5c782239497129666f53efb2f9284ed3
Nandhagopal2912 <naanu2912@gmail.com>
2026-03-28 16:44:09 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+import math
+str1 = "ABCABC"
+str2 = "ABC"
+
+load_dotenv()
+
+def gcdOfStrings(str1, str2):
+    if str1 + str2 != str2 + str1:
+        return ""
+    
+    gcd_len = math.gcd(len(str1), len(str2))
+    return str1[:gcd_len]
+
+print(gcdOfStrings(str1, str2))
+
+print(os.getenv("SECRET_KEY"))
